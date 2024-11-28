cd85f2edede1b9c70ee866954724d83983a87a85
Andy Chao <zhaoqing@spon.com.cn>
2024-11-28 13:58:20 +0800

    Initial Commit

---
+++ SwiftUIMusicApp/SwiftUIMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMusicAppApp.swift
+//  SwiftUIMusicApp
+//
+//  Created by Andy Chao on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
